2b5da667b96c50319195396193fb2addcbe6cd37
Elise Young <eliseyoung_4@hotmail.com>
2017-04-25 16:37:50 +1000

    add tests with rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Post, type: :model do
     expect(post).to_not be_valid
   end
 
+  it "post requires title" do
+    post = Post.create(:title => nil)
+    expect(post).to_not be_valid
+  end
+
+  it "post requires title of atleast 2 characters" do
+    post = Post.create(:title => "a")
+    expect(post).to_not be_valid
+  end
+  
 end
