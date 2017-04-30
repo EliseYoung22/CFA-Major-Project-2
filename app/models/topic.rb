ffc85a38c491c584951f11f2c03b1db9028dd24b
Elise Young <eliseyoung_4@hotmail.com>
2017-04-30 17:56:24 +1000

    add topic model tests

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,4 +7,7 @@ class Topic < ApplicationRecord
   has_attached_file :cover_art, styles: { medium: "512x512>", thumb: "100x100>" }, :default_url => "/infinity.png", :storage => :cloudinary, :path => ":id/:style/:filename"
   validates_attachment_content_type :cover_art, content_type: /\Aimage\/.*\z/
 
+  validates :name, presence: true
+  validates :description, presence: true, length: {minimum: 2}
+
 end
